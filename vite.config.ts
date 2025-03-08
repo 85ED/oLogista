5e3981ba4c31b526f96c96b374fbf9a8659e0df3
85ED <edsonfelixnet@gmail.com>
2025-03-07 21:20:43 -0300

    SafeMode

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/o-lojista/',
+  base: '/olojista/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
